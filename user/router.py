8a547fc763c49471520db6f40c1f66b5cd1984f1
bhw-rgb <banghyeonu@gmail.com>
2026-04-09 23:13:33 +0900

    파일 수정

--- user/router.py
+++ user/router.py
@@ -1,7 +1,7 @@
 # 1) DB랑 Session을 진행하기 위해서 SessionFactory 불러온다.
 # 2) user.models에서 User를 불러온다. -> User 객체는 DeclarativeBase 상속받음 -> orm은 Python 코드로 SQL에 요청함.
 # 3) router에 session을 넣는다(공장) -> user.models에 있는 User 객체를 가져온다
-# 4) User 객체는 Python 응답을 SQL로 보내서 데이터를 만
+# 4) User 객체는 Python 응답을 SQL로 보내서 데이터를 요청, 응답
 
 from fastapi import APIRouter, Path, Query, status, HTTPException, Depends
 from sqlalchemy import select, delete
@@ -17,26 +17,26 @@ router = APIRouter(prefix="/users", tags=["User"])
 
 # GET /users
 @router.get(
-        "",
-        summary="전체 사용자 데이터 조회 API",
-        status_code=status.HTTP_200_OK,
-        response_model=list[UserResponse]
-        )
+    "",
+    summary="전체 사용자 데이터 조회 API",
+    status_code=status.HTTP_200_OK,
+    response_model=list[UserResponse]
+    )
 def get_users_handler(session = Depends(get_session)):
-        # statement = 구문(명령문)
-        stmt = select(User)                     # SELECT * FROM user;
-        result = session.execute(stmt)
-        users = result.scalars().all()          # mappings() -> 딕셔너리로 가져온다. # scalars() -> 맨 첫번째 값만 가져온다.
+    # statement = 구문(명령문)
+    stmt = select(User)                     # SELECT * FROM user;
+    result = session.execute(stmt)
+    users = result.scalars().all()          # mappings() -> 딕셔너리로 가져온다. # scalars() -> 맨 첫번째 값만 가져온다.
 
-        return users
+    return users
 
 
 # GET /users/search?name=alex
 @router.get(
-        "/search",
-        summary="사용자 데이터 검색 API",
-        response_model=list[UserResponse]
-        )
+    "/search",
+    summary="사용자 데이터 검색 API",
+    response_model=list[UserResponse]
+    )
 def search_user_handler(
     name: str | None = Query(None), 
     job: str | None = Query(None), 
@@ -59,14 +59,14 @@ def search_user_handler(
 
 # GET /users/{user_id}
 @router.get(
-        "/{user_id}",
-        summary="단일 사용자 데이터 조회 API",
-        response_model=UserResponse
-        )
+    "/{user_id}",
+    summary="단일 사용자 데이터 조회 API",
+    response_model=UserResponse
+    )
 def get_user_handler(
     user_id: int = Path(..., ge=1),
-    session = Depends(get_session),
-    ):                                          # Depends(get_session) -> SQLAlchemy 의존성 주입
+    session = Depends(get_session),             # Depends(get_session) -> SQLAlchemy 의존성 주입
+    ):                                          
     
     stmt = select(User).where(User.id == user_id)
     result = session.execute(stmt)
@@ -79,28 +79,28 @@ def get_user_handler(
 
 # POST /users
 @router.post(
-        "",
-        summary="사용자 데이터 추가 API",
-        status_code=status.HTTP_201_CREATED,
-        response_model=UserResponse
-        )
+    "",
+    summary="사용자 데이터 추가 API",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserResponse
+    )
 def create_user_handler(
     body: UserCreateRequest, 
     session = Depends(get_session),             # Depends(get_session) -> SQLAlchemy 의존성 주입
     ):
     new_user = User(name=body.name, job=body.job)
-    session.add(new_user)                       # Session 데이터 추가하라고 DB 한테 말함
-    session.commit()                            # 변경사항 저장
+    session.add(new_user)                       # Session 데이터 추가하라고 DB 한테 말함 (등록 예약)
+    session.commit()                            # 변경사항 저장 (진짜 등록)
     session.refresh(new_user)                   # id, created_at 읽어옴 -> DB랑 FastAPI서버를 동기화한다.
     return new_user
 
 
 # PATCH /users/{user_id}
 @router.patch(
-        "/{user_id}",
-        summary="사용자 데이터 수정 API",
-        response_model=UserResponse,
-        )
+    "/{user_id}",
+    summary="사용자 데이터 수정 API",
+    response_model=UserResponse,
+    )
 def update_user_handler(
     user_id: int, 
     body: UserUpdateRequest, 
@@ -123,16 +123,16 @@ def update_user_handler(
 # DELETE /user/{user_id}
 # 메세지를 넣어도 204 때문에 메세지 안뜬다.
 @router.delete(
-        "/{user_id}",
-        summary="사용자 데이터 삭제 API",
-        status_code=status.HTTP_204_NO_CONTENT,
-        )
+    "/{user_id}",
+    summary="사용자 데이터 삭제 API",
+    status_code=status.HTTP_204_NO_CONTENT,
+    )
 def delete_user_handler(
     user_id:int, 
     session = Depends(get_session),             # Depends(get_session) -> SQLAlchemy 의존성 주입
     ): 
 
-    # 1) 조회하고 삭제하는 법 & 의존성 주입 전 코드
+    # 1) 의존성 주입 전 코드 | 조회하고 삭제하는법
     # with SessionFactory as session:
     #     stmt = select(User).Where(User.id == user_id)
     #     result = session.execute(stmt)
